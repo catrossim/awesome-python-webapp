42b4f775c1bd71a7f127cdc7afa757e40a161d92
catrossim <catrossim@gmail.com>
2016-09-09 16:58:40 +0800

    add config settings

---
+++ www/config.py
@@ -0,0 +1,40 @@
+# config.py
+
+import config_default
+
+class Dict(dict):
+    def __init__(self, names=(), values=(), **kw):
+        super(Dict, self).__init__(**kw)
+        for k, v in zip(names, values):
+            self[k] = v
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+    def __setattr__(self, key, value):
+        self[key] = value
+
+def merge(default, override):
+    result = Dict()
+    for k, v in default.iteritems():
+        if k in override:
+            if isinstance(v, dict):
+                result[k] = merge(v, override=[k])
+            else:
+                result[k] = override[k]
+        else:
+            result[k] = v
+    return result
+
+configs = config_default.configs
+
+try:
+    import config_override
+    configs = merge(configs, config_override.configs)
+except ImportError:
+    pass
+
+if __name__=='__main__':
+    print configs
---
+++ www/config_default.py
@@ -0,0 +1,14 @@
+# config_default.py
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'test',
+        'password': 'test',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
